4fb49e7850042e7265c722348568a9553e3c31fb
Nipun Bhardwaj <Nipun Bhardwaj>
2022-01-25 14:43:02 +0530

    .

--- api/api.go
+++ api/api.go
@@ -1,4 +1,4 @@
-package api
+package Api
 
 import "fmt"
 
--- mod1.go
+++ mod1.go
@@ -3,12 +3,12 @@ package mod1
 import (
 	"fmt"
 
-	"github.com/nipunbhardwaj/go_ms_sub_module_1/api"
+	"github.com/nipunbhardwaj/go_ms_sub_module_1/Api"
 )
 
 func SecretProcess() {
 	fmt.Println("Running the secret process!")
-	item := api.Item{
+	item := Api.Item{
 		Label: "Inside Item",
 	}
 	item.PrintLabel()
